766ead51f51cfbb47b3b5907b3f5714477f2ca8f
RoninsWork <yurkaronin@gmail.com>
2022-10-19 18:51:46 +0300

    Сверстал и оживил первый слайд с картинками

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@ menuButton.addEventListener('click', function () {
   document.body.classList.toggle('mob-menu-open');
   menuButton.classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiperIMG = new Swiper('.slider__images', {
+    // параметры слайдера
+    loop: false,
+    speed: 2400,
+    parallax: true,
+    autoplay: true,
+    mousewheel: {
+      invert: false,
+    },
+  });
+});
